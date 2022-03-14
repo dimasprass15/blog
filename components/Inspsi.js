9748c6292d63795298a8d1fe4cb1d706d635b93e
dimasprass15 <coretan.boi@gmail.com>
2022-03-14 23:01:57 +0700

    second commit

--- components/Inspsi.js
+++ components/Inspsi.js
@@ -5,9 +5,9 @@ export default function Inspsi() {
             <p className='Down'>
 
                 <small>
-                    <strong>Sebaik-baik kalian adalah orang yang mempelajari Al Qur'an dan mengajarkannya. (HR Bukhari 5027 )</strong>
+                    <strong>Sebaik-baik kalian adalah orang yang mempelajari Al Quran dan mengajarkannya. (HR Bukhari 5027 )</strong>
                     <br />My Inspiration :
-                    <br />Muhammad Shallalahu 'Alaihi Wassalam
+                    <br />Muhammad Shallalahu Alaihi Wassalam
                 </small>
 
             </p>
